dee0f8d4700f5fca13131450bb68ef8c794787f9
Andrea Papaluca <andreapapaluca@gmail.com>
2023-03-30 12:08:21 +1100

    introduced huggingfacePipeline and service context

--- kg_construction.py
+++ kg_construction.py
@@ -19,7 +19,7 @@ args = parser.parse_args()
 
 assert len(args.infiles) > 0
 
-docs = []
+#docs = []
 nodes = []
 for i,infile in enumerate(args.infiles):
     with open(infile, 'r') as f:
@@ -27,36 +27,48 @@ for i,infile in enumerate(args.infiles):
         chunks = [ Node(text=c, doc_id=str(i)) for c in chunks if len(c) > 0 ]
         for c in chunks:
             nodes.append(c)
-        docs.append(Document(f.read()))
+        #docs.append(Document(f.read()))
 
-from llm import CustomLLM
+#from llm import CustomLLM
 import torch
 from transformers import pipeline as Pipeline
+from langchain.llms import HuggingFacePipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from llama_index.indices.service_context import ServiceContext
+
+
+device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+print(f'> Using device: {device}.')
 
 #model = "facebook/opt-iml-max-30b"
 #model = "google/t5-v1_1-base"
 #model = "EleutherAI/gpt-j-6B"
-model = "gpt2"
+model_id = "gpt2"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(model_id)
 pipeline = "text-generation"
 #pipeline = "text2text-generation"
-device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
-pipeline = Pipeline(
+
+pipe = Pipeline(
     pipeline,
     model=model,
+    tokenizer=tokenizer,
+    max_new_tokens=10,
     device=device,
     model_kwargs={"torch_dtype":torch.bfloat16}
 )
-print(f'> Using device: {device}.')
+
+llm = HuggingFacePipeline(pipeline=pipe)
+
 print(f' # Model: {model}\n # Pipeline: {pipeline}')
 
 # define LLM
 llm_predictor = LLMPredictor(
-    llm = CustomLLM(
-        model = model,
-        pipeline = pipeline,
-        out_tokens = 256
-    )
+    llm = llm
 )
+
+service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, chunk_size_limit=512)
+
 
 print('---------------')
 
@@ -71,7 +83,7 @@ else:
         nodes=nodes,
         chunk_size_limit=512, 
         max_triplets_per_chunk=7,
-        llm_predictor=llm_predictor,
+        service_context=service_context
     )
     index.save_to_disk('index_kg.json')
 
