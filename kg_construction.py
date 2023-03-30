3f0b56fc1cc66b8b3900118212507272350bc6c3
BrunoLiegiBastonLiegi <andreapapaluca@gmail.com>
2023-03-31 10:23:33 +1100

    minor changes

--- kg_construction.py
+++ kg_construction.py
@@ -11,23 +11,26 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 from llama_index import LLMPredictor
 from llama_index.indices.knowledge_graph.base import GPTKnowledgeGraphIndex
 from llama_index import Document
+from llama_index.data_structs.node_v2 import Node
+
 
 parser = argparse.ArgumentParser(description='KG Construction.')
 parser.add_argument('infiles', nargs='+')
 parser.add_argument('--load_index')
 args = parser.parse_args()
 
-assert len(args.infiles) > 0
+if args.load_index is None:
+    assert len(args.infiles) > 0
 
-#docs = []
-nodes = []
-for i,infile in enumerate(args.infiles):
-    with open(infile, 'r') as f:
-        chunks = f.read().split('\n')
-        chunks = [ Node(text=c, doc_id=str(i)) for c in chunks if len(c) > 0 ]
-        for c in chunks:
-            nodes.append(c)
-        #docs.append(Document(f.read()))
+    #docs = []
+    nodes = []
+    for i,infile in enumerate(args.infiles):
+        with open(infile, 'r') as f:
+            chunks = f.read().split('\n')
+            chunks = [ Node(text=c, doc_id=str(i)+'-'+str(j)) for j,c in enumerate(chunks) if len(c) > 0 ]
+            for c in chunks:
+                nodes.append(c)
+                #docs.append(Document(f.read()))
 
 #from llm import CustomLLM
 import torch
@@ -37,7 +40,12 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from llama_index.indices.service_context import ServiceContext
 
 
-device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+if torch.cuda.is_available():
+    device = torch.device('cuda:0')    
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device('cpu')
 print(f'> Using device: {device}.')
 
 #model = "facebook/opt-iml-max-30b"
@@ -60,7 +68,7 @@ pipe = Pipeline(
 
 llm = HuggingFacePipeline(pipeline=pipe)
 
-print(f' # Model: {model}\n # Pipeline: {pipeline}')
+print(f' # Model: {model_id}\n # Pipeline: {pipeline}')
 
 # define LLM
 llm_predictor = LLMPredictor(
@@ -69,19 +77,17 @@ llm_predictor = LLMPredictor(
 
 service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, chunk_size_limit=512)
 
-
 print('---------------')
 
 if args.load_index is not None:
-    index = GPTKnowledgeGraphIndex.load_from_disk(args.load_index, llm_predictor=llm_predictor)
     print(f'> Loading Knowledge Graph from `{args.load_index}`.')
+    index = GPTKnowledgeGraphIndex.load_from_disk(args.load_index, service_context=service_context)
 else:
     print('> Creating the Knowldge Graph.')
     # NOTE: can take a while! 
     index = GPTKnowledgeGraphIndex(
         #docs,
         nodes=nodes,
-        chunk_size_limit=512, 
         max_triplets_per_chunk=7,
         service_context=service_context
     )
@@ -99,7 +105,7 @@ response = index.query(
 print(response)
 """
 
-query = "Show me the triples extracted from the input documents"
+query = "where is Karnataka located?"
 print(f'\n\n-------> {query} :\n')
 response = index.query(
     query, 
