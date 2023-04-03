9a137e4f9485eb5927c4805e255e84145268230a
Andrea Papaluca <andreapapaluca@gmail.com>
2023-04-03 12:15:19 +1000

    custom prompt + testing big models

--- kg_construction.py
+++ kg_construction.py
@@ -30,13 +30,12 @@ if args.load_index is None:
             chunks = [ Node(text=c, doc_id=str(i)+'-'+str(j)) for j,c in enumerate(chunks) if len(c) > 0 ]
             for c in chunks:
                 nodes.append(c)
-                #docs.append(Document(f.read()))
+            #docs.append(Document(f.read()))
 
-#from llm import CustomLLM
 import torch
 from transformers import pipeline as Pipeline
 from langchain.llms import HuggingFacePipeline
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig, AutoModelForSeq2SeqLM
 from llama_index.indices.service_context import ServiceContext
 
 
@@ -49,11 +48,29 @@ else:
 print(f'> Using device: {device}.')
 
 #model = "facebook/opt-iml-max-30b"
-#model = "google/t5-v1_1-base"
-#model = "EleutherAI/gpt-j-6B"
-model_id = "gpt2"
+#model_id = "google/t5-v1_1-base"
+model_id = "EleutherAI/gpt-j-6B"
+#model_id = "gpt2"
+
 tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForCausalLM.from_pretrained(model_id)
+#config = AutoConfig.from_pretrained(model_id)
+
+#from accelerate import init_empty_weights, load_checkpoint_and_dispatch
+
+#with init_empty_weights():
+#model = AutoModelForCausalLM.from_config(config)
+#model.tie_weights()
+model = AutoModelForCausalLM.from_pretrained(model_id, revision="float16")
+#model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
+"""
+model = load_checkpoint_and_dispatch(
+    model,
+    model_id,
+    device_map="auto",
+    #no_split_module_classes=["GPTJBlock"]
+)
+"""
+
 pipeline = "text-generation"
 #pipeline = "text2text-generation"
 
@@ -61,13 +78,15 @@ pipe = Pipeline(
     pipeline,
     model=model,
     tokenizer=tokenizer,
-    max_new_tokens=10,
-    device_map='auto', # used for distributed
-    #device=device,
-    model_kwargs={"torch_dtype":torch.bfloat16}
+    #max_new_tokens=256,
+    device=device,
+    #device_map='auto', # used for distributed
+    #model_kwargs={"torch_dtype":torch.bfloat16}
 )
 
 llm = HuggingFacePipeline(pipeline=pipe)
+
+from llama_index.prompts.prompts import KnowledgeGraphPrompt
 
 kg_extraction_template =  (
     "Some text is provided below. Given the text, extract up to "
@@ -86,6 +105,10 @@ kg_extraction_template =  (
     "Text: {text}\n"
     "Triplets:\n"
 )
+prompt = KnowledgeGraphPrompt(
+    kg_extraction_template
+)
+
 
 print(f' # Model: {model_id}\n # Pipeline: {pipeline}')
 
@@ -96,29 +119,51 @@ llm_predictor = LLMPredictor(
 
 service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, chunk_size_limit=512)
 
-print('---------------')
+print('--------------------------------------------------------------')
 
 if args.load_index is not None:
     print(f'> Loading Knowledge Graph from `{args.load_index}`.')
-    # not working since there is no `index_struct` key in the index dict
-    index = GPTKnowledgeGraphIndex.load_from_disk(args.load_index, service_context=service_context)
+    index = GPTKnowledgeGraphIndex.load_from_disk(
+        args.load_index,
+        service_context=service_context
+    )
 else:
     print('> Creating the Knowldge Graph.')
-    # NOTE: can take a while! 
+    nodes = nodes[:10]
+    print('> Input text:')
+    for n in nodes:
+        print(n.text)
     index = GPTKnowledgeGraphIndex(
-        #docs,
         nodes=nodes,
+        kg_triple_extract_template=prompt,
         max_triplets_per_chunk=7,
         service_context=service_context
     )
     index.save_to_disk('index_kg.json')
 
+print('\n---------- Visualize the extracted KG ----------\n')
+import networkx as nx
+import matplotlib.pyplot as plt
+from pyvis.network import Network
+
+g = index.get_networkx_graph()
+net = Network(notebook=True, cdn_resources="in_line", directed=True)
+net.from_nx(g)
+net.show("example.html")
+"""
+kg = index.get_networkx_graph() 
+nx.draw(kg)
+plt.show()
+for e in kg.edges(data=True):
+    print(e)
+"""
+
 query = "where is Karnataka located?"
 print(f'\n\n-------> {query} :\n')
 response = index.query(
     query, 
-    include_text=False, 
-    response_mode="tree_summarize"
+    #include_text=False, 
+    #response_mode="tree_summarize"
 )
 
 print(response)
